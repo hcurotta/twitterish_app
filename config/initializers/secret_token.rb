95e0a15cd15b4bfeb25e0f40baa62e6e44bdc49b
hcurotta <hcurotta@gmail.com>
2012-07-17 23:19:37 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterishApp::Application.config.secret_token = '11b40cec2b4d3fcc5918e7db5a81a8c3bf909a35e36dde6f6146130bb3dab330957d854ca60457ced18dd80371cf24edd2a1fbf13a41ff26a5e3c11f2727fdbf'
